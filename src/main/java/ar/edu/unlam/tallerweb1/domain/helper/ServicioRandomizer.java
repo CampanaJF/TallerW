0c4acc1cceef5b88e31ba8eb82558534ecca9d62
CampanaJF <jucampana@alumno.unlam.edu.ar>
2022-11-28 10:05:33 -0300

    post sprint

--- src/main/java/ar/edu/unlam/tallerweb1/domain/helper/ServicioRandomizer.java
+++ src/main/java/ar/edu/unlam/tallerweb1/domain/helper/ServicioRandomizer.java
@@ -6,28 +6,26 @@ import org.springframework.stereotype.Service;
 
 @Service("servicioRandomizer")
 public class ServicioRandomizer {
-	
+
 	public int obtenerIndice(Integer indiceMax) {
-		
+
 		Random r = new Random();
 		int low = 0;
 		int high = indiceMax;
-		int resultado = r.nextInt(high-low);
-		
+		int resultado = r.nextInt(high - low);
+
 		return resultado;
 	}
 
-	
-	public int obtenerIndice(Integer indiceMax,Integer indice) {
-	
-	Integer resultado = obtenerIndice(indiceMax);
-	
-	while(resultado==indice) {
-		resultado = obtenerIndice(indiceMax);
-	}
-		
-	return resultado;
-	}
+	public int obtenerIndice(Integer indiceMax, Integer indiceAnterior) {
 
+		Integer resultado = obtenerIndice(indiceMax);
+
+		while (resultado == indiceAnterior) 
+			resultado = obtenerIndice(indiceMax);
+		
+
+		return resultado;
+	}
 
 }
